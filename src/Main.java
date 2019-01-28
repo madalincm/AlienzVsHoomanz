5a3701a908b4e7c3791be34032395bbc0f74fac9
Madalin Cotetiu <cotetiumadalin@gmail.com>
2019-01-28 15:23:17 +0200

    final test

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,15 @@ public class Main {
         Human human1 = new Human();
         Alien alien1 = new Alien();
 
-        Human human2 = new Human();
-        System.out.println(alien1.performAttack(human2));
-        System.out.println(alien1.performAttack(human2));
-        System.out.println(alien1.performAttack(human2));
-        System.out.println(alien1.performAttack(human2));
-        System.out.println(alien1.performAttack(human2));
-        System.out.println(alien1.performAttack(human2));
-        human2.displayStats();
-
-        System.out.println(alien1.performAttack(human2));
-        System.out.println(human2.performAttack(alien1));
-
-        System.out.println(human1.performAttack(alien1));
-        System.out.println(human1.performAttack(alien1));
-        System.out.println(human1.performAttack(alien1));
-        System.out.println(human1.performAttack(alien1));
-        System.out.println(human1.performAttack(alien1));
-        System.out.println(human1.performAttack(alien1));
-        System.out.println(human1.performAttack(alien1));
-        System.out.println(human1.performAttack(alien1));
+        human1.displayStats();
+        alien1.displayStats();
+        System.out.println(alien1.performAttack(human1));
+        human1.displayStats();
         alien1.displayStats();
 
+        System.out.println(human1.performAttack(alien1));
+        System.out.println(human1.performAttack(alien1));
+        human1.displayStats();
+        alien1.displayStats();
     }
 }
